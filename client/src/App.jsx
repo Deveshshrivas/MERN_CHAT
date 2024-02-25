4abde83960281d5c774642698ffab4f2583de8b0
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2024-02-25 10:56:49 +0530

    committed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return<BrowserRouter>
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<Home/>} />
       <Route path="*" element={<h1>Not Found</h1>} />
       <Route path="/about" element={<h1>About</h1>} />
     </Routes>
