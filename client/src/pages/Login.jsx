0c9fe4a6cb2a93be1f235084bcb0da7defce67a2
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2024-02-25 12:57:18 +0530

    commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { TextField, Container, Paper, Typography, Button} from '@mui/material';
+import { TextField, Container, Paper, Typography, Button, Stack, Avatar } from '@mui/material';
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
-  const toggleLogin = () => setIsLogin(false);
+  const toggleLogin = () => setIsLogin((prev) => !prev);
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xs" sx={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
       <Paper elevation={3} sx={{
         padding: 4,
         display: 'flex',
@@ -30,7 +35,30 @@ const Login = () => {
               </form>
             </>
           ) : (
-            <span>register</span>
+            <>
+              <Typography variant="h5">Sign Up</Typography>
+              <form style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}>
+              <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+                <Avatar sx={{
+                  width: "10rem",
+                  height: "10rem",
+                  objectFit: "contain"
+                }}
+                />
+              </Stack>
+              <TextField required fullWidth label='Name' margin='normal' variant='outlined' />
+              <TextField required fullWidth label='UserName' margin='normal' variant='outlined' />
+              <TextField required fullWidth label='Bio' margin='normal' variant='outlined' />
+              <TextField required fullWidth label='password' type='password' margin='normal' variant='outlined' />
+              <Button type='submit' variant='contained' color='primary' fullWidth sx={{marginTop: "1rem"}}>Sign Up</Button>
+
+              <Typography textAlign={'center'} m={"1rem"}>OR</Typography>
+              <Button variant='contained' color='secondary' fullWidth sx={{marginTop: "1rem"}} onClick={toggleLogin}> Login Instead </Button> 
+              </form>
+            </>
           )
         }
       </Paper>
