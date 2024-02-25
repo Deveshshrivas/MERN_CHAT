09a576d52db97f93dcf472a950ab590ea69119ea
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2024-02-25 11:32:24 +0530

    committed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Container, Paper } from '@mui/material';
 
 const Login = () => {
   return (
-    <div>login</div>
-  )
+    <Container component={"main"} maxWidth="xs">
+      <Paper elevation={3} sx={{
+        padding: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+      </Paper>
+    </Container>
+  );
 }
 
-export default Login
+export { Login };
